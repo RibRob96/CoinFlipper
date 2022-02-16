63a65019ab09f8b00c86d9a2a8ca4c1e3184fab7
Rob Harwood <43217009+RibRob96@users.noreply.github.com>
2022-02-16 17:37:09 -0500

    Fixed get element by Id error

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementByID("btn");
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
     //Get a random number between 0 and 3.
+    console.log("Button was pressed!");
     const randomNumber = 2;
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
